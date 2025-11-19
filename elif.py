e0abdd350b996df7d8e287e65312e6d1a6dd9164
Vigneshwarans11 <vigneshsrinivasan1111@gmail.com>
2025-11-19 11:00:01 +0530

    commit_elif

---
+++ elif.py
@@ -0,0 +1,81 @@
+# check lower or upper or digit or spl char
+# a=input("enter the char:")
+# if ('A'<=a<='Z'):
+#     print("uppercase")
+# elif('a'<=a<='z'):
+#     print("lowercase")
+# elif('0'<=a<='9'):
+#     print("digit")
+# else:
+#     print("spl char")
+
+# check given digit is 1/2/3 or morethan3
+# value=input("enter the value:")
+# if (len(value)==1):
+#     print("single")
+# elif(len(value)==2):
+#     print("double")
+# elif(len(value)==3):
+#     print("3 digit")
+# else:
+#     print("more than 3 digit")
+
+
+# check which quardent is point
+# x= int(input("enter the x-axis:"))
+# y= int(input("enter the y-axis:"))
+# if (x>0 and y>0):
+#     print("1st quadrent")
+# elif(x<0 and y>0):
+#     print("2nd Quadrent")
+# elif(x<0 and y<0):
+#     print("3rd quadrent")
+# elif(x>0 and y<0):
+#     print("4th Quadrent")
+# else:
+#     print("points in origin")
+
+# find the greatest of 3 num
+# a=int(input("num1:"))
+# b=int(input("num2:"))
+# c=int(input("num3:"))
+# if(a>b and a>c):
+#     print("A is greater")
+# elif(b>a and b>c):
+#     print("B is greater")
+# else:
+#     print("C is greater")
+
+# relation b/w two integer num
+# a=int(input("num1:"))
+# b=int(input("num2:"))
+# if(a>b):
+#     print(a,"is grater than",b)
+# elif(b>a):
+#     print(b,"is greater than",a)
+# else:
+#     print(a,"is equal to",b)
+
+#Consider a character input if it is uppercase convert it into lowercase, if it is lowercase convert it into uppercase, if it is digit print the reminder when it is divided by 3 else if it is special character print It's ASCII value.
+
+# a=input("enter the char:")
+# if ('A'<=a<='Z'): 
+#     print(chr(ord(a)+32))
+# elif ('a'<=a<="z"):
+#     print(chr(ord(a)-32))
+# elif ('0'<=a<='9'):
+#     print(int(a)%3)
+# else:
+#     print(ord(a))
+
+# Wap to print 'Fizz' if the given number is multiple of three,. print 'buzz' if the given number is multiple of 5 and print Fizzbuss' if the number is multiple ofboth 3 and 5
+# a=int(input("enter the value:"))
+# if (a%3==0 and a%5==0):
+#     print("FizzBuzz")
+# elif(a%3==0):
+#     print("Fizz")
+# elif(a%5==0):
+#     print("Buzz")
+# else:
+#     print("nothing")
+
