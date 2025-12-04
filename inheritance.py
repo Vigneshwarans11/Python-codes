4e4c7d4dc5dd6c1622921c831cb9a69c1f5d250f
Vigneshwarans11 <vigneshsrinivasan1111@gmail.com>
2025-12-04 11:06:42 +0530

    inheritence file

--- inheritance.py
+++ inheritance.py
@@ -94,7 +94,167 @@
 # obj1=whatsapp1("vignesh","bjjbj")
 # obj1.display_1()
 
+#inheritence
+# class animals():
+#     def sound(self):
+#         print("makes sound")
+# class dog(animals):
+#     def sound(self):
+#         print("dog barks")
+# d=dog()
+# d.sound()
 
-        
+#single level inheritence
+# class parent():
+#     def display(self):
+#         print("parent class")
+# class child(parent):
+#     def show(self):
+#         print("child class")
+# obj=child()
+# obj.display()
+# obj.show()
+
+# #single level inheritence using __init__
+# class bank():
+#     def __init__(self,b_name,b_ifsc):
+#         self.b_name=b_name
+#         self.b_ifsc=b_ifsc
+#     def display_bank(self):
+#         print(self.b_name,self.b_ifsc)
+# class account(bank):
+#     def __init__(self,b_name,b_ifsc,c_name,c_age):
+#         super().__init__(b_name,b_ifsc)
+#         self.c_name=c_name
+#         self.c_age=c_age
+#     def display_acc(self):
+#         print(self.c_name,self.c_age)
+# acc=account("SBI",3433,"vignesh",23)
+# acc.display_bank()
+# acc.display_acc()
+
+# multi_level inheritence
+# class bank():
+#     def __init__(self,b_name,b_ifsc):
+#         self.b_name=b_name
+#         self.b_ifsc=b_ifsc
+#     def display_bank(self):
+#         print(self.b_name,self.b_ifsc)
+# class account(bank):
+#     def __init__(self,b_name,b_ifsc,a_num,a_type):
+#         super().__init__(b_name,b_ifsc)
+#         self.a_num=a_num
+#         self.a_type=a_type
+#     def display_acc(self):
+#         super().display_bank()
+#         print(self.a_num,self.a_type)
+
+# class customer(account):
+#     def __init__(self,b_name,b_ifsc,a_num,a_type,c_name,c_age):
+#         super().__init__(b_name,b_ifsc,a_num,a_type)
+#         self.c_name=c_name
+#         self.c_age=c_age
+#     def display_customer(self):
+#         super().display_acc()
+#         print(self.c_name,self.c_age)
+# obj=customer("hadf",6464,5545,"savings","vicky",21)
+# # obj.display_bank()
+# # obj.display_acc()
+# obj.display_customer()
+
+
+# Multiple Inheritance
+# class bank():
+#     def __init__(self,b_name):
+#         self.b_name=b_name
+#     def display_bank(self):
+#         print(self.b_name)
+# class customer():
+#     def __init__(self,c_name):
+#         self.c_name=c_name
+#     def display_customer(self):
+#         print(self.c_name)
+
+# class account(bank,customer):
+#     def __init__(self,b_name,c_name,a_type):
+#         bank.__init__(self,b_name)
+#         customer.__init__(self,c_name)
+#         self.a_type=a_type
+#     def display_acc(self):
+#         self.display_bank()
+#         self.display_customer()
+#         print(self.a_type)
+
+# obj=account("hgjbdj","bdjsbd","djbs")
+# obj.display_acc()
+
+
+# Hierarchical Inheritance
+# class bank():
+#     def __init__(self,b_name):
+#         self.b_name=b_name
+#     def display_bank(self):
+#         print(self.b_name)
+# class account(bank):
+#     def __init__(self,b_name,acc_name):
+#         super().__init__(b_name)
+#         self.acc_name= acc_name
+#     def display_acc(self):
+#         print(self.acc_name)
+
+# class customer(bank):
+#     def __init__(self,b_name,c_name):
+#         super().__init__(b_name)
+#         self.c_name=c_name
+#     def display_customer(self):
+#         print(self.c_name)
+
+# obj1=account("hdfc","saving")
+# obj2=customer("hdfc","vignesh")
+
+# obj1.display_bank()
+# obj1.display_acc()
+
+# obj2.display_bank()
+# obj2.display_customer()
+
+
+
+# Hybrid Inheritance
+# Combination of more than one inheritance type
+# Parent class
+# class Bank:
+#     def bank_info(self):
+#         print("Bank: HDFC")
+
+# # Level 1 child
+# class Customer(Bank):
+#     def cust_info(self):
+#         print("Customer: Vignesh")
+
+# # Another Level 1 child
+# class LoanDetails:
+#     def loan_info(self):
+#         print("Loan amount: 50000")
+
+# # Hybrid child class (inherits from Customer + LoanDetails)
+# class Account(Customer, LoanDetails):
+#     def acc_info(self):
+#         print("Account Type: Savings")
+
+# # Object
+# obj = Account()
+# obj.bank_info()
+# obj.cust_info()
+# obj.loan_info()
+# obj.acc_info()
+
+
+
+
+
+
+
+
 
         
