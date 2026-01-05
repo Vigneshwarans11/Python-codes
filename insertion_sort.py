cb1cf1385cbd33ca6c8d7fd080131451c9757f68
Vigneshwarans11 <vigneshsrinivasan1111@gmail.com>
2026-01-05 07:36:20 +0530

    changes

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,10 +1,36 @@
+# arr = [12, 11, 13, 5, 6]
+# for i in range (1,len(arr)):
+#     key=arr[i]
+#     j=i-1
+
+#     while j>=0 and arr[j]>key:
+#         arr[j+1]=arr[j]
+#         j=j-1
+#     arr[j+1]=key
+# print(arr)
+
+
+# arr = [12, 11, 13, 5, 6]
+# n=len(arr)
+# for i in range(1,n):
+#     key=arr[i]
+#     j=i-1
+
+#     while j>=0 and arr[j]>key:
+#         arr[j+1] = arr[j]
+#         j=j-1
+#         arr[j+1]=key
+# print(arr)
+
+
 arr = [12, 11, 13, 5, 6]
-for i in range (1,len(arr)):
+n=len(arr)
+for i in range(1,n):
     key=arr[i]
     j=i-1
 
     while j>=0 and arr[j]>key:
         arr[j+1]=arr[j]
         j=j-1
-    arr[j+1]=key
+        arr[j+1]=key
 print(arr)
