14d175cc2b486f0c5a6dc101e1e812a443f498c4
Vigneshwarans11 <vigneshsrinivasan1111@gmail.com>
2026-06-05 21:49:28 +0530

    Basics

---
+++ Basics.py
@@ -0,0 +1,31 @@
+# a=10.5
+# b=3
+# print(a//b)
+# # help()
+# a=-10
+# print(a//b)
+
+# a=4+5j
+# b=2
+# print(a*b)
+# c="mss "
+# print(b*c)
+# a=(input("enter the number:"))
+# b=int(float(a))
+# print(b,type(b))
+
+# a=bool(input("value:"))
+# print(a,type(a))
+
+# a=10.1
+# b=10.1
+# print(a,b)
+# print(a is b)
+# print(a is not b)
+
+c=[10,20,30]
+print(10 in c)
+print(10 not in c)
+
+
+
