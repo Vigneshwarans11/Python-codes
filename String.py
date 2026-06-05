2b80e7c450580b80df73f091db35602d66723241
Vigneshwarans11 <vigneshsrinivasan1111@gmail.com>
2026-06-05 22:12:11 +0530

    string

---
+++ String.py
@@ -0,0 +1,18 @@
+# s="i am from TamilNadu"
+# print(s.swapcase()) #I AM FROM tAMILnADU
+# s="we are a student"
+# print(s.capitalize()) #We are a student
+# s1="We are a student"
+# print(s.casefold())
+
+# s2="my name is vignesh"
+# print(s2.count("a"))
+# print(s2.count("i",-6))
+# print(s2.count("oo"))
+
+# s3="What bro it's very wrong bro"
+# print(s3.index('r'))
+# print(s3.index("it's"))
+# print(s3.rindex("r"))
+# print(s3.index("z"))
+
